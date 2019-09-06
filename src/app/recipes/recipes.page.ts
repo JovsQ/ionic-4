bdf667d8f8a4fce1944801be2148b63332b82e7c
Jovel Vasquez Quiamco <jovel@monstar-lab.ph>
2019-09-06 14:45:07 +0800

    Recipes

---
+++ src/app/recipes/recipes.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.page.html',
+  styleUrls: ['./recipes.page.scss'],
+})
+export class RecipesPage implements OnInit {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'http://reporter.ph/wp-content/uploads/2017/10/larry-gadon.jpg',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/homemade-spaghetti-sauce-horizontal-1530890913.jpg?crop=1xw:1xh;center,top&resize=480:*',
+      ingredients: ['Spaghetti', 'Pork Meat', 'Tomatoes']
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
